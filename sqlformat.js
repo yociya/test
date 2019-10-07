5a71a9ccf23cd1f974a14ab53d8f9517fc717535
yociya <yociya.n@gmail.com>
2019-10-07 17:03:02 +0900

    Update sqlformat.js

--- sqlformat.js
+++ sqlformat.js
@@ -1,7 +1,7 @@
 document.querySelector('#format').addEventListener('click', buttonAction);
 document.querySelector('#bfsql').focus();
 document.execCommand('paste');
-    
+
 function buttonAction(event) {
     var $bfsql = document.querySelector('#bfsql');
     var bfsqltext = $bfsql.value;
@@ -12,78 +12,112 @@ function buttonAction(event) {
     document.execCommand('copy');
 }
 
-function format(sql){
+function format(sql) {
     var result = sql;
-    result = result.replace(/\n/g,' ');
-    result = result.replace(/ +/g,' ');
-    result = result.replace(/select /ig,'SELECT ');
-    result = result.replace(/from /ig,'FROM ');
-    result = result.replace(/where /ig,'WHERE ');
-    result = result.replace(/order /ig,'ORDER ');
-    result = result.replace(/by /ig,'BY ');
-    result = result.replace(/and /ig,'AND ');
-    result = result.replace(/or /ig,'OR ');
-    
-    result = result.replace(/AND\(/g,'AND (');
-    result = result.replace(/OR\(/g,'OR (');
+    result = result.replace(/\n/g, ' ');
+    result = result.replace(/ +/g, ' ');
+    result = result.replace(/select /ig, 'SELECT ');
+    result = result.replace(/distinct /ig, 'DISTINCT ');
+    result = result.replace(/from /ig, 'FROM ');
+    result = result.replace(/where /ig, 'WHERE ');
+    result = result.replace(/order /ig, 'ORDER ');
+    result = result.replace(/by /ig, 'BY ');
+    result = result.replace(/and /ig, 'AND ');
+    result = result.replace(/or /ig, 'OR ');
 
-    result = result.replace(/SELECT DISTINCT /g,  'SELECT DISTINCT\n      ');
-    result = result.replace(/FROM /g           ,'\n  FROM\n       ');
-    result = result.replace(/WHERE /g          ,'\n WHERE\n       ');
-    result = result.replace(/AND /g            ,'\n   AND ');
-    result = result.replace(/OR /g             ,'\n    OR ');
-    result = result.replace(/ORDER BY /g       ,'\n ORDER BY ');
-    result = select(result,0);
+    result = result.replace(/to_number\(/ig, 'TO_NUMBER(');
+    result = result.replace(/get_val\(/ig, 'GET_VAL(');
+
+    result = result.replace(/AND\(/g, 'AND (');
+    result = result.replace(/OR\(/g, 'OR (');
+
+    result = result.replace(/SELECT /g, 'SELECT\n      ');
+    result = result.replace(/SELECT\n      DISTINCT /g, 'SELECT DISTINCT\n      ');
+    result = result.replace(/FROM /g, '\n  FROM\n       ');
+    result = result.replace(/WHERE /g, '\n WHERE\n       ');
+    result = result.replace(/AND /g, '\n   AND ');
+    result = result.replace(/OR /g, '\n    OR ');
+    result = result.replace(/ORDER BY /g, '\n ORDER BY ');
+    result = select(result, 0);
+    result = from(result, 0);
     return result;
-    
-    function select(target,index){
-        if(index < 0) {
+
+    function select(target, index) {
+        if (index < 0) {
             return target;
         }
-        var selectstart = target.indexOf('SELECT ',index);
-        if(selectstart < 0) {
+        var selectstart = target.indexOf('SELECT ', index);
+        if (selectstart < 0) {
             return target;
         }
-        var fromstart = target.indexOf(' FROM\n',selectstart);
-        var af = target.substring(fromstart,target.length);
-        var newsql = cutReplace(target,/,/g,'\n     ,',selectstart,fromstart);
-        newsql = bracketsOpen(newsql,selectstart,fromstart);
+        var fromstart = searchOpenClose(target, selectstart, /SELECT /g, ' FROM\n');
+        var af = target.substring(fromstart, target.length);
+        var newsql = cutReplace(target, /,/g, '\n     ,', selectstart, fromstart);
+        newsql = bracketsOpen(newsql, selectstart, fromstart);
         var nextindex = newsql.indexOf(af);
-        return select(newsql,nextindex);
+        return select(newsql, nextindex);
     }
-    function bracketsOpen(s,index,endindex) {
-        var openindex = s.indexOf('(',index);
-        if(openindex < 0 || endindex < openindex) {
+
+    function from(target, index) {
+        if (index < 0) {
+            return target;
+        }
+        var fromstart = target.indexOf(' FROM\n', index);
+        if (fromstart < 0) {
+            return target;
+        }
+        var wherestart = searchOpenClose(target, fromstart, / FROM\n/g, ' WHERE\n');
+        if (wherestart < 0) {
+            wherestart = searchOpenClose(target, fromstart, / FROM\n/g, ' ORDER BY ');
+            if (wherestart < 0) {
+                wherestart = target.length;
+            }
+        }
+        var af = target.substring(wherestart, target.length);
+        var newsql = cutReplace(target, /,/g, '\n     ,', fromstart, wherestart);
+        newsql = bracketsOpen(newsql, fromstart, wherestart);
+        var nextindex = newsql.indexOf(af);
+        if (wherestart == target.length) {
+            return newsql;
+        }
+        return from(newsql, nextindex);
+    }
+
+    function bracketsOpen(s, index, endindex) {
+        var openindex = s.indexOf('(', index);
+        if (openindex < 0 || endindex < openindex) {
             return s;
         }
-        var closeindex = searchBracketsClose(s,openindex);
+        var closeindex = searchOpenClose(s, openindex, /\(/g, ')');
         var commaindex = s.indexOf('\n     ,');
-        if(commaindex < 0){
+        if (commaindex < 0) {
             return s;
         }
-        if(closeindex > commaindex) {
-            var news = cutReplace(s,/\n     ,/g,',',openindex,closeindex);
-            return bracketsOpen(news,closeindex+1,endindex);
+        if (closeindex > commaindex) {
+            var news = cutReplace(s, /\n     ,/g, ',', openindex, closeindex);
+            return bracketsOpen(news, closeindex + 1, endindex);
         }
-        return bracketsOpen(s,closeindex+1,endindex);
+        return bracketsOpen(s, closeindex + 1, endindex);
     }
-    function cutReplace(target,search,reptext,st,en) {
-        var rep = target.substring(st,en);
-        rep = rep.replace(search,reptext);
-        var bf = target.substring(0,st);
-        var af = target.substring(en,target.length);
+
+    function cutReplace(target, search, reptext, st, en) {
+        var rep = target.substring(st, en);
+        rep = rep.replace(search, reptext);
+        var bf = target.substring(0, st);
+        var af = target.substring(en, target.length);
         var newsql = bf + rep + af;
         return newsql;
     }
-    function searchBracketsClose(s,openindex) {
-        var closeindex = s.indexOf(')',openindex);
-        var ss = s.substring(openindex+1,closeindex);
-        var ar = ss.match(/\(/g);
-        if(ar == null) {
+
+    function searchOpenClose(s, openindex, opentag, closetag) {
+        var closeindex = s.indexOf(closetag, openindex);
+        var ss = s.substring(openindex + 1, closeindex);
+        var ar = ss.match(opentag);
+        if (ar == null) {
             return closeindex;
         } else {
-            for (var i=0;i<ar.length;i++) {
-                closeindex = s.indexOf(')',closeindex+1);
+            for (var i = 0; i < ar.length; i++) {
+                closeindex = s.indexOf(closetag, closeindex + 1);
             }
             return closeindex;
         }
